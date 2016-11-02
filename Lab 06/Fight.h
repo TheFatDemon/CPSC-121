25391d74cfc0394c4945de1b7d3aab90cabe462a
Mario Segura <its@thefatdemon.me>
2016-11-02 12:01:33 -0700

    Added Base Files

---
+++ Lab 06/Fight.h
@@ -0,0 +1,31 @@
+//Eric May
+//CS 121 Spring 16
+//Declares a Fight class that facilitates combat between two participants
+
+//ifndef wrapper included so multiple inclusions doesn't cause errors
+#ifndef _Fight_H
+#define _Fight_H
+
+#include <string>
+struct Combatant {
+	int curHealth, maxHealth, attackMin, attackRange, winCount;
+	std::string name;
+};
+
+class Fight {
+	//Only accessible within the class
+private:
+	Combatant foes[2];//Contains the two combatants doing combat
+	int turn;//Counts how many turns have passed in the battle
+	int takeTurn();//Returns 1 if fight continues, 2 if combatant 1 wins, 3 if fighter 2 wins. Displays details.
+	void generateFighter(Combatant &fighter);//Fills in the passed combatant with random stats and user chosen name.
+											 //MaxHealth: 20-40   AttackMin: 3-6	AttackRange: 5-10
+
+											 //Usable whenever variable of type is declared, using . operator
+public:
+	Fight();//Constructor initializes values upon declaration
+	void getFighters();//Initializes the fighters before a fight begins
+	void runFight();//Begins a fight until one participant is defeated, displaying information about events.
+};
+
+#endif
--- Lab 09/Main.cpp
+++ Lab 09/Main.cpp
@@ -1,9 +1,34 @@
+//Eric May
+//CS 121 Spring 16
+//Includes our other files and briefly uses them
 #include <iostream>
+#include <cstdlib>
+#include <time.h>
+#include "Fight.h"
 
 using namespace std;
 
-int main() {
-	cout << "Hello World" << endl;
+int main()
+{
+	srand(time(NULL));
+	Fight brawl;
+	brawl.getFighters();
+	int choice;
+	do
+	{
+		brawl.runFight();
+		std::cout << "Would you like a rematch?\n1. Yes\t2. No\n";
+		std::cin >> choice;
+		if (choice == 1)
+			continue;
+		else
+		{
+			std::cout << "Would you like to host another fight?\n1. Yes\t2. No\n";
+			std::cin >> choice;
+			if (choice == 1)
+				brawl.getFighters();
+		}
+	} while (choice != 2);
 	system("pause");
 	return 0;
 }
