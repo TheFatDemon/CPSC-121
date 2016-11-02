ab527b0cf423e0b5740838a907ffbf5f72d5f4c7
Mario Segura <its@thefatdemon.me>
2016-11-02 12:11:42 -0700

    Created Skeleton for implementation

---
+++ Lab 09/Fight.cpp
@@ -0,0 +1,21 @@
+#include "Fight.h"
+
+Fight::Fight() {
+
+}
+
+int Fight::takeTurn() {
+	return 0;
+}
+
+void Fight::generateFighter(Combatant &fighter) {
+
+}
+
+void Fight::getFighters() {
+
+}
+
+void Fight::runFight() {
+
+}
