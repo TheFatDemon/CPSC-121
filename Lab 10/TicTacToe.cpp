11c848db983eb34f1a9c2d31f814da86b60956e8
Mario <its@thefatdemon.me>
2016-12-07 13:25:12 -0800

    Fixed constructor

--- Lab 10/TicTacToe.cpp
+++ Lab 10/TicTacToe.cpp
@@ -4,10 +4,36 @@
 using namespace std;
 
 TicTacToe::TicTacToe() {
-
+	for (int x = 0; x < 3; x++) {
+		for (int y = 0; y < 3; y++) {
+			board[x][y] = 0;
+		}
+	}
+	nextPlayer = 1;
 }
 
 int TicTacToe::checkWin() {
+
+	// We are checking if the last player won
+	int lastPlayer = 0;
+	if (nextPlayer == 2) {
+		lastPlayer = 1;
+	}
+	else {
+		lastPlayer = 2;
+	}
+
+
+	// Check for 
+	// x
+	// x
+	// x
+	// Strategy: We know that in a vertical win, at least one column needs to be populated
+	for (int i = 0; i < 3; i++)
+	{
+		if (board[i][0] != lastPlayer && board[i][0]);
+	}
+
 	return 0;
 }
 
@@ -16,7 +42,7 @@ void TicTacToe::displayBoard() {
 	cout << "+-+-+-+" << endl;
 	for (int i = 0; i < 3; i++) {
 		cout << "|";
-		for (int j = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
 			cout << board[i][j] << "|";
 		}
 		cout << endl;
@@ -32,7 +58,7 @@ int TicTacToe::makeMove(int position) {
 
 	// We now need to get the x and y of the position, relative to the board
 	int x = position / 3; // Ex: 5 / 3 = 1
-	int y = position % 3; // Ex: 3 % 3 = 2
+	int y = position % 3; // Ex: 5 % 3 = 2
 
 	// Now that we have X and Y, we check the board if the value is in there already
 	if (board[x][y] == 1 || board[x][y] == 2) {
@@ -45,7 +71,7 @@ int TicTacToe::makeMove(int position) {
 	}
 
 	// Indicate next player
-	// If nextPlayer = 2, set it to 2, else set it to 1
+	// If nextPlayer = 1, set it to 2, else set it to 1
 	if (nextPlayer == 2) {
 		nextPlayer = 1;
 	}
